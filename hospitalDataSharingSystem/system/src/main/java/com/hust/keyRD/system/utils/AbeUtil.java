cd26834828049ce85403bd8e47aede40f55cff9a
ZhaoWenHao1 <happyzhao1010@gmail.com>
2021-09-09 23:25:57 +0800

    初步完成文件加解密，待测试

--- hospitalDataSharingSystem/system/src/main/java/com/hust/keyRD/system/utils/AbeUtil.java
+++ hospitalDataSharingSystem/system/src/main/java/com/hust/keyRD/system/utils/AbeUtil.java
@@ -1,20 +1,20 @@
 package com.hust.keyRD.system.utils;
 
-import cn.hutool.core.io.resource.ResourceUtil;
 import com.hust.keyRD.system.file.model.FileModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 import sg.edu.ntu.sce.sands.crypto.dcpabe.*;
 import sg.edu.ntu.sce.sands.crypto.dcpabe.ac.AccessStructure;
-import sg.edu.ntu.sce.sands.crypto.utility.Utility;
+import sg.edu.ntu.sce.sands.crypto.dcpabe.key.PersonalKey;
+import sg.edu.ntu.sce.sands.crypto.dcpabe.key.PublicKey;
+import sg.edu.ntu.sce.sands.crypto.dcpabe.key.SecretKey;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @program: hospitalDataSharingSystem
@@ -23,6 +23,7 @@ import java.io.IOException;
  * @create: 2021/9/9 10:42
  **/
 @Component
+@Slf4j
 public class AbeUtil {
 
     @Autowired
@@ -32,76 +33,182 @@ public class AbeUtil {
     private final String akSecKeyName = "akSecKey";
     private final String akPubKeyName = "akPubKey";
 
-//    @Value("${dcpabe.filePath}")
+    //    @Value("${dcpabe.filePath}")
 //    @Value("classpath:/abe/")
     public static String rootPath;
 
-    public static GlobalParameters gp;
-    private static PublicKeys publicKeys;
-    private static String gpPath;
-    private static String akSecKeyPath;
-    private static String akPubKeyPath;
+    public static volatile GlobalParameters gp;
+    //    private static volatile PublicKeys publicKeys;
+    private static volatile Map<String, PublicKey> publicKeys;
+    private static volatile Map<String, SecretKey> secretKeys;
 
-//    @PostConstruct
+//    private static String gpPath;
+//    private static String akSecKeyPath;
+//    private static String akPubKeyPath;
+
+    //    @PostConstruct
     public void init() throws IOException, ClassNotFoundException {
         rootPath = AbeUtil.getRootPath();
-        gpPath = rootPath + "abe/dcpabe.gp";
-        akSecKeyPath = rootPath + "abe/akSecKey";
-        akPubKeyPath = rootPath + "abe/akPubKey";
         initGp();
     }
 
 
+    /**
+     * 声明全部属性  并将全部属性声明后的公私钥序列化上传到monogo
+     *
+     * @param attrs 全部属性
+     * @return
+     * @throws IOException
+     */
     public AuthorityKeys authoritySetUp(String[] attrs) throws IOException {
+        if (gp == null) {
+            initGp();
+        }
+
         AuthorityKeys ak = DCPABE.authoritySetup("authority", gp, attrs);
 
-        Utility.writeSecretKeys(akSecKeyPath, ak.getSecretKeys());
-        Utility.writePublicKeys(akPubKeyPath, ak.getPublicKeys());
+        mongoDbUtil.upload(Objects.requireNonNull(SerializeUtil.serialize(ak.getPublicKeys())), akPubKeyName);
+        mongoDbUtil.upload(Objects.requireNonNull(SerializeUtil.serialize(ak.getSecretKeys())), akSecKeyName);
 
-        publicKeys.subscribeAuthority(ak.getPublicKeys());
-        // todo upload到 monogoDb
+        publicKeys = ak.getPublicKeys();
+        secretKeys = ak.getSecretKeys();
         return ak;
     }
 
-//    public void encrypt(byte[] data, String policy){
-//        Message message = new Message(data);
-//        AccessStructure as = AccessStructure.buildFromPolicy(policy);
-//        DCPABE.encrypt(message,)
-//    }
+    /**
+     * 将数据根据policy进行加密
+     *
+     * @param data   要加密的数据
+     * @param policy 加密策略
+     * @return
+     */
+    public Ciphertext encrypt(byte[] data, String policy) {
+        Message message = new Message(data);
+        AccessStructure as = AccessStructure.buildFromPolicy(policy);
+        if (gp == null) {
+            initGp();
+        }
+        if (publicKeys == null) {
+            initPublicKeys();
+        }
+        PublicKeys pKeys = new PublicKeys();
+        pKeys.subscribeAuthority(publicKeys);
+        Ciphertext ciphertext = DCPABE.encrypt(message, as, gp, pKeys);
+        return ciphertext;
+    }
+
+    /**
+     * 授权用户属性
+     *
+     * @param attrs
+     * @param username
+     */
+    public void grantAttrToUser(String[] attrs, String username) {
+        if (gp == null) {
+            initGp();
+        }
+        if (secretKeys == null) {
+            initSecretKeys();
+        }
+
+        for (String attr : attrs) {
+            PersonalKey personalKey = DCPABE.keyGen(username, attr, secretKeys.get(attr), gp);
+            String fileName = generateNameByUserAndAttr(username, attr);
+            mongoDbUtil.upload(Objects.requireNonNull(SerializeUtil.serialize(personalKey)), fileName);
+        }
+    }
+
+    public Message decrypt(Ciphertext ct, String username, String[] attrs) {
+        if (gp == null) {
+            initGp();
+        }
+
+        PersonalKeys personalKeys = new PersonalKeys(username);
+        for (String attr : attrs) {
+            String fileName = generateNameByUserAndAttr(username, attr);
+            FileModel fileModel = mongoDbUtil.getByName(fileName);
+            if (fileModel != null) {
+                PersonalKey pk = (PersonalKey) SerializeUtil.unserialize(fileModel.getContent().getData());
+                personalKeys.addKey(pk);
+            } else {
+                log.warn(String.format("decrypt error: attr[{}] did not be granted", attr));
+                throw new RuntimeException();
+            }
+        }
+
+        Message message = DCPABE.decrypt(ct, personalKeys, gp);
+        return message;
+
+    }
 
 
-    public static String getRootPath(){
-        try{
+    public static String getRootPath() {
+        try {
             return ResourceUtils.getURL("classpath:").getPath();
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             return "";
         }
     }
 
-    public void initGp() throws IOException, ClassNotFoundException {
-        FileModel fileModel = mongoDbUtil.getByName("dcpabe.gp");
-        if(fileModel == null){
-            System.out.println("generate");
-            gp = DCPABE.globalSetup(160);
-            mongoDbUtil.upload(SerializeUtil.serialize(gp), "dcpabe.gp");
-        }else {
-            System.out.println("unSerialize");
-            gp = (GlobalParameters) SerializeUtil.unserialize(fileModel.getContent().getData());
+    /**
+     * 初始化全局gp  如果mongoDb中有，则反序列化，否则生成并上传到mongoDB
+     */
+    public void initGp() {
+        if (gp == null) {
+            synchronized (AbeUtil.class) {
+                if (gp == null) {
+                    try {
+                        FileModel fileModel = mongoDbUtil.getByName(gpName);
+                        if (fileModel == null) {
+                            log.info("generate GlobalParameters");
+                            gp = DCPABE.globalSetup(160);
+                            mongoDbUtil.upload(Objects.requireNonNull(SerializeUtil.serialize(gp)), "dcpabe.gp");
+                        } else {
+                            log.info("unSerialize GlobalParameters");
+                            gp = (GlobalParameters) SerializeUtil.unserialize(fileModel.getContent().getData());
+                        }
+                    } catch (Exception e) {
+                        log.warn("gp init error: " + e.getMessage());
+                    }
+
+                }
+            }
         }
-//        rootPath = AbeUtil.getRootPath();
-//        // 从本地读
-//        // to do ： 从monogodb中读
-//        File gpFile = new File(rootPath + "abe/dcpabe.gp");
-//        if(gpFile.exists()){
-//            gp = Utility.readGlobalParameters(gpFile.getPath());
-//        }else {
-//            gp = DCPABE.globalSetup(160);
-//
-//            Utility.writeGlobalParameters(gpFile.getPath(), gp);
-//        }
     }
 
-    public static String generateNameByUserAndAttr(String username, String[] attrs){
+    public void initPublicKeys() {
+        if (publicKeys == null) {
+            synchronized (AbeUtil.class) {
+                if (publicKeys == null) {
+                    FileModel fileModel = mongoDbUtil.getByName(akPubKeyName);
+                    if (fileModel != null) {
+                        log.info("unSerialize PublicKeys");
+                        publicKeys = (Map<String, PublicKey>) SerializeUtil.unserialize(fileModel.getContent().getData());
+                    } else {
+                        log.warn("PublicKeys init error: file is not exist");
+                    }
+                }
+            }
+        }
+    }
+
+    public void initSecretKeys() {
+        if (secretKeys == null) {
+            synchronized (AbeUtil.class) {
+                if (secretKeys == null) {
+                    FileModel fileModel = mongoDbUtil.getByName(akSecKeyName);
+                    if (fileModel != null) {
+                        log.info("unSerialize SecretKeys");
+                        secretKeys = (Map<String, SecretKey>) SerializeUtil.unserialize(fileModel.getContent().getData());
+                    } else {
+                        log.warn("SecretKeys init error: file is not exist");
+                    }
+                }
+            }
+        }
+    }
+
+    public static String generateNameByUserAndAttr(String username, String[] attrs) {
         StringBuilder sb = new StringBuilder();
         sb.append("abeAttr#");
         sb.append(username);
@@ -109,6 +216,15 @@ public class AbeUtil {
             sb.append("#");
             sb.append(attr);
         }
+        return sb.toString();
+    }
+
+    public static String generateNameByUserAndAttr(String username, String attr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("abeAttr#");
+        sb.append(username);
+        sb.append("#");
+        sb.append(attr);
         return sb.toString();
     }
 }
